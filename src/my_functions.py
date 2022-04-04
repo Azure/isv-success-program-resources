ef7fef8c3a444a6dc85b2826cd38000656939297
David Starr <david@elegantcode.com>
2022-04-04 08:37:53 -0700

    now outputs line numbers for each instance

--- src/my_functions.py
+++ src/my_functions.py
@@ -1,17 +1,15 @@
 from email import contentmanager
 from importlib.resources import contents
 from logging import RootLogger
-
-
 import os
 from re import sub
 
 # walk the folder structure from root
-def process_md_files(rootDir):
+def process_md_files(root_dir):
     
-    print('Walking: ' + os.path.abspath(rootDir))
+    print('Walking: ' + os.path.abspath(root_dir))
 
-    for dirpath, dirnames, files in os.walk(rootDir):
+    for dirpath, dirnames, files in os.walk(root_dir):
         for item in files:
             if item.endswith(".md"):
                 fileNamePath = str(os.path.join(dirpath,item))
@@ -20,15 +18,14 @@ def process_md_files(rootDir):
 
 # operate on a single file
 def process_md_file(filepath):
-    print(filepath)
 
+    print(filepath)
     file = open(filepath, "r")
     
+    line_count = 0
     for line in file.readlines():
-        ll = line.lower()
-        is_in_line = ("no-content" in ll)
-
-        if is_in_line:
-            print(ll)
+        line_count += 1
+        if ("no-" in line.lower()):
+            print("Line: " + str(line_count) + " " + line, end="")
     
     file.close()
